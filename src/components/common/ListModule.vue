1f2ac00b9bf3d3bbd532454fd1d6617808ca465c
Chiko <2452559902@qq.com>
2025-06-22 11:34:14 +0800

    fixed:修改 git 为不忽略大小写

---
+++ src/components/common/ListModule.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="list-module" :class="[boxStyle, outStyle]">
+        <span class="list-head" :class="[headStyle]" @click="triggerFun">{{ head }}</span>
+        <div class="list-content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+<script lang='ts' setup>
+import { computed } from 'vue';
+interface ListModule {
+    head: string;
+    boxStyle?: string;
+    headStyle?: string;
+    outStyle?: string;
+    headColor?: string;
+    gapColor?: string;
+    gapHeight?: string;
+}
+const props = withDefaults(defineProps<ListModule>(), {
+    headColor: 'rgba(74,74,74)',
+    gapColor: 'rgba(254,236,239)',
+    gapHeight: "22px"
+});
+const emits = defineEmits(['turn']);
+const outStyle = computed(() => props.outStyle ? props.outStyle : "");
+const triggerFun = () => emits('turn');
+
+</script>
+<style lang='scss' scoped>
+.list-module {
+    width: 100%;
+    border-radius: 15px;
+    padding: 15px;
+    box-sizing: border-box;
+    background-color: white;
+
+    .list-head {
+        display: inline-block;
+        color: v-bind('props.headColor');
+
+        &::before {
+            content: '';
+            display: inline-block;
+            width: 2.5px;
+            height: v-bind(gapHeight);
+            background: v-bind('props.gapColor');
+            vertical-align: text-top;
+            margin-right: 10px;
+        }
+    }
+
+    .list-content {
+        margin-top: 10px;
+    }
+}
+
+.dashed {
+    border: 1px dashed black !important;
+}
+
+.wdashed {
+    border: 2px dashed black !important;
+}
+
+.head-point {
+    cursor: pointer;
+    font-weight: 600;
+
+    &:hover {
+        color: rgb(255, 117, 117);
+    }
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="login">
+        <div class="land-module">
+            <span class="login-tips">打开手机网易云APP 扫码登录</span>
+            <img :src="qrSrc || loginm">
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+import { loginKey, loginQrCheck, loginQrCreate } from '@/api';
+import { onMounted, onUnmounted, ref } from 'vue';
+import { storage } from '@/util';
+import { usePlay } from '@/store/play';
+import loginm from "@/assets/image/login_m.png";
+
+const router = useRouter();
+const qrSrc = ref<string>("");
+const play = usePlay();
+let timer: number | undefined;
+
+async function qrLogin() {
+    const time = Date.now();
+    const { data } = await loginKey(time);
+    const { data: qrData } = await loginQrCreate(data.unikey, true, time);
+    qrSrc.value = qrData.qrimg;
+    qrCheck(data.unikey);
+}
+
+async function qrCheck(key: string) {
+    timer && clearInterval(timer);
+    timer = window.setInterval(() => {
+        const time = Date.now();
+        loginQrCheck(key, time).then(res => {
+            if (res.code == 802) {
+                const userInfo = { avatarUrl: res.avatarUrl, nickname: res.nickname };
+                storage.set("loginInfo", userInfo);
+                play.$patch({ userInfo: userInfo });
+            }
+            if (res.code == 803) {
+                storage.set('cookie', res.cookie);
+                storage.set('isLogin', true);
+                router.replace({ name: 'discover' });
+                play.$patch({ isLogin: true });
+                timer && clearInterval(timer);
+            }
+        }).catch((e) => {
+            console.error(e, 'login fail =====>');
+        })
+    }, 2500);
+}
+
+onMounted(async () => {
+    await qrLogin();
+})
+onUnmounted(() => {
+    timer && clearInterval(timer);
+});
+
+</script>
+
+<style lang="scss" scoped>
+.login {
+    position: relative;
+    background-color: $color;
+    width: 100%;
+    height: 100vh;
+
+    .land-module {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        width: 400px;
+        height: 600px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+
+        .login-tips {
+            margin-bottom: 10px;
+        }
+    }
+}
+</style>
